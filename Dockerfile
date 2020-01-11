ae84a516d52d7dc5095c8f18c612fa21370fb76c
Josef Reichardt <reichardt.josef+gitlab@gmail.com>
2020-01-11 23:59:30 +0100

    Fix & Refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM certbot/certbot:latest
 MAINTAINER Josef Reichardt
 
-RUN apk add --no-cache curl
+RUN apk add --no-cache curl gettext
 
 COPY entrypoint.sh /entrypoint.sh
+COPY secret-patch.template.json /secret-patch.template.json
 
 ENTRYPOINT "/entrypoint.sh"
--- entrypoint.sh
+++ entrypoint.sh
@@ -15,23 +15,25 @@ fi
 
 echo "Start certificate update for domains $DOMAINS (email: $EMAIL)"
 
-echo ""
 echo "Run certbot ..."
-certbot certonly -d "$DOMAINS" --standalone -n --agree-tos -m "$EMAIL"
+certbot certonly -d "$DOMAINS" --standalone -n --agree-tos -m "$EMAIL" || exit 1
 
-echo ""
 echo "Looking for certificates ..."
 CERTPATH=/etc/letsencrypt/live/$(echo "$DOMAINS" | cut -f1 -d',')
 ls "$CERTPATH" || exit 1
 
+export NAME="$SECRET"
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+export NAMESPACE
 echo "Updating secret \"$SECRET\" (namespace: $NAMESPACE) ..."
-{
-  echo "{\"kind\":\"Secret\",\"apiVersion\":\"v1\","
-  echo "\"metadata\":{\"name\":\"$NAME\",\"namespace\":\"$NAMESPACE\"},"
-  echo "\"data\":{\"tls.crt\":\"$(base64 "${CERTPATH}/fullchain.pem" | tr -d '\n')\","
-  echo "\"tls.key\":\"$(base64 "${CERTPATH}/privkey.pem" | tr -d '\n')\"},\"type\":\"Opaque\"}"
-} > /secret-patch.json
+FULLCHAIN=$(base64 "${CERTPATH}/fullchain.pem" | tr -d '\n')
+export FULLCHAIN
+PRIVKEY=$(base64 "${CERTPATH}/privkey.pem" | tr -d '\n')
+export PRIVKEY
+envsubst < /secret-patch.template.json > /secret-patch.json
+echo "--- secret:"
+cat /secret-patch.json
+echo "---"
 curl \
   --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
   -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
