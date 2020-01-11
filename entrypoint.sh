303e08a2c8d2b96beb482d331e4b3bf8adab3860
Josef Reichardt <reichardt.josef+gitlab@gmail.com>
2020-01-12 00:12:57 +0100

    dry-run

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,24 +16,27 @@ fi
 echo "Start certificate update for domains $DOMAINS (email: $EMAIL)"
 
 echo "Run certbot ..."
-certbot certonly -d "$DOMAINS" --standalone -n --agree-tos -m "$EMAIL" || exit 1
+certbot certonly --dry-run -d "$DOMAINS" --standalone -n --agree-tos -m "$EMAIL" || exit 1
 
 echo "Looking for certificates ..."
 CERTPATH=/etc/letsencrypt/live/$(echo "$DOMAINS" | cut -f1 -d',')
 ls "$CERTPATH" || exit 1
 
-export NAME="$SECRET"
 NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-export NAMESPACE
 echo "Updating secret \"$SECRET\" (namespace: $NAMESPACE) ..."
+
 FULLCHAIN=$(base64 "${CERTPATH}/fullchain.pem" | tr -d '\n')
-export FULLCHAIN
 PRIVKEY=$(base64 "${CERTPATH}/privkey.pem" | tr -d '\n')
+
+export NAME="$SECRET"
+export NAMESPACE
+export FULLCHAIN
 export PRIVKEY
 envsubst < /secret-patch.template.json > /secret-patch.json
 echo "--- secret:"
 cat /secret-patch.json
 echo "---"
+
 curl \
   --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
   -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
